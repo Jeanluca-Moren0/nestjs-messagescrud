8ff334020a0ae1b743d5b1bac6c55abe40fe5a38
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-12-05 20:51:18 -0300

    refactor: removing unutilised archives from now on

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { MessagesModule } from './messages/messages.module';
+
 
 // Ponto de partida, onde colocamos os controllers disponiveis e quem ira fornecer: providers
 
