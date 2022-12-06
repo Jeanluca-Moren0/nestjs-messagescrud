32976f0e43871c76ccaa3f46b364ae33ffc84ac8
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-12-05 21:03:07 -0300

    feat: add pokemon endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 
 
 // Ponto de partida, onde colocamos os controllers disponiveis e quem ira fornecer: providers
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, PokemonModule],
   controllers: [AppController],
   providers: [AppService],
 })
