58e4fc939d32e7b28f26748453bee1ce03c4a1b6
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-12-05 22:29:14 -0300

    feat: pokemon create DTO

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,7 @@
-export class CreatePokemonDto {}
+import { Prisma } from '@prisma/client';
+
+export class CreatePokemonDto implements Prisma.PokemonsCreateInput {
+  name: string;
+  height?: number;
+  Images?: Prisma.ImagesCreateNestedManyWithoutPokemonInput;
+}
